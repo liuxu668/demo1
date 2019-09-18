39fae91e33de26abe6d5570213dfe588f56a7a3e
liuxu668 <2458554813@qq.com>
2019-09-18 14:29:07 +0800

    第一次提交

---
+++ src/main/java/org/java/demo1/Demo.java
@@ -0,0 +1,10 @@
+package org.java.demo1;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("111111111");
+        System.out.println("111111111");
+    }
+
+}
